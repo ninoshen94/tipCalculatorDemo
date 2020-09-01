c6094486b342f4790e55cbad872783b6cfd393d0
Ruoyu Shen <ninoshen94@gmail.com>
2020-09-01 18:04:11 -0400

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Ruoyu Shen on 9/1/20.
+//  Copyright © 2020 Ruoyu Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
